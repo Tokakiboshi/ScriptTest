db75e7365d9762db9f902094b7a06ed46c83a4b7
Tokakiboshi <84213099+Tokakiboshi@users.noreply.github.com>
2021-06-05 21:34:50 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+
+    {
+        int[] array = new int[5];
+
+        array[0] = 5;
+        array[1] = 4;
+        array[2] = 3;
+        array[3] = 2;
+        array[4] = 1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        
+
+        for (int i =4 ; i > -1; i--)
+
+        {
+            Debug.Log(array[i]);
+        }
+            }
+
+            void Update()
+            {
+
+            }
+    }
